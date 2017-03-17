d0f6150c66dc0c119b845f0ee7d34597df2bb085
omar montoya  pedro piza <montoyaomar01@hotmail.com pedropiza@fluvip.com>
2017-03-17 15:14:07 -0500

    finish

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,58 +1,65 @@
-def quality_smaller_than_fifty(item)
-  range_quality = (1..49).to_a
-  range_quality.include?(item.quality) && item.quality += 1
-end
-
-def validate_increment_quality(item)
-  item.quality += 1
-  item.name == 'Backstage passes to a TAFKAL80ETC concert' && evaluate_sell_and_quality(item)
-end
-
-def evaluate_sell_and_quality(item)
-  (-10..10).to_a.include?(item.sell_in) && quality_smaller_than_fifty(item)
-  (-10..5).to_a.include?(item.sell_in) && quality_smaller_than_fifty(item)
-end
-
-def block3(item)
-  item.name != "Aged Brie" && block4(item) || quality_smaller_than_fifty(item)
-end
-
-def block4(item)
-  block6(item) || subtrack_quality_if_possible(item)
-end
-
-def subtrack_quality_if_possible(item)
-  ((9..80).to_a.include?(item.quality) && item.name != 'Sulfuras, Hand of Ragnaros' && (item.quality -= 1))
-end
-
-def block6(item)
-  item.name == 'Backstage passes to a TAFKAL80ETC concert' && (item.quality = 0) 
-end
-
-def block7(item)
-  item.name == 'NORMAL ITEM' || item.name == 'Sulfuras, Hand of Ragnaros'
-end
-
-def block8(item)
-  (1..80).to_a.include?(item.quality) && item.name != 'Sulfuras, Hand of Ragnaros' && item.quality -= 1
-end
-
-def block9(item)
-  (1..49).to_a.include?(item.quality) && validate_increment_quality(item)
-end
-
-def start(item)
-  block7(item) && block8(item) || block9(item)
-  item.name != 'Sulfuras, Hand of Ragnaros' &&  item.sell_in -= 1
-  (-11..-1).to_a.include?(item.sell_in) && block3(item)
-end
-
 def update_quality(items)
   items.each do |item|
     start(item)
   end
 end
 
+private
+def start(item)
+  validate_name(item) && quality_greater_than_zero(item) || validate_increment_quality(item)
+  item.name != 'Sulfuras, Hand of Ragnaros' &&  item.sell_in -= 1
+  item.sell_in < 0 && change_value_quality(item)
+end
+
+def validate_name(item)
+  item.name == 'NORMAL ITEM' || item.name == 'Sulfuras, Hand of Ragnaros'
+end
+
+def quality_greater_than_zero(item)
+  item.quality > 0 && reduce_quality(item)
+end
+
+def reduce_quality(item)
+  item.name != 'Sulfuras, Hand of Ragnaros' && item.quality -= 1
+end
+
+def validate_increment_quality(item)
+  quality_on_range?(item) && increment_quality(item)
+end
+
+def quality_on_range?(item)
+  (1..49).to_a.include?(item.quality)
+end
+
+def change_value_quality(item)
+  item.name != "Aged Brie" && zero_or_reduce_quality(item) || quality_smaller_than_fifty(item)
+end
+
+def zero_or_reduce_quality(item)
+  assign_zero_to_quality(item) || quality_greater_than_eigth(item)
+end
+
+def assign_zero_to_quality(item)
+  item.name == 'Backstage passes to a TAFKAL80ETC concert' && (item.quality = 0) 
+end
+
+def quality_greater_than_eigth(item)
+  item.quality > 8 && reduce_quality(item)
+end
+
+def quality_smaller_than_fifty(item)
+  item.quality <50 && item.quality += 1
+end
+
+def increment_quality(item)
+  item.quality += 1
+  item.name == 'Backstage passes to a TAFKAL80ETC concert' && evaluate_sell_and_quality(item)
+end
+
+def evaluate_sell_and_quality(item)
+  item.sell_in < 11 && quality_smaller_than_fifty(item)
+  item.sell_in < 6 && quality_smaller_than_fifty(item)
+end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
